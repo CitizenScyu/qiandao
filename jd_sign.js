bf4eafecc3bbfcd7e480c33348c0b2752fcaf841
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2021-04-30 10:16:40 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -11,7 +11,7 @@ const download = require('download')
 const cookie = process.env.JD_COOKIE
 // 京东Cookie
 const dual_cookie = process.env.JD_DUAL_COOKIE
-const dual_cookie = process.env.JD_DUAL_COOKIE2
+const dual_cookie2 = process.env.JD_DUAL_COOKIE2
 
 // Server酱SCKEY
 const push_key = process.env.PUSH_KEY
